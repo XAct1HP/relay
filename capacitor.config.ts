4d75617b97a5fa2526e8812cd36e87647bf0cbba
XAct1HP <trickylion05@gmail.com>
2026-03-16 23:57:47 -0400

    another potential fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Relay',
   webDir: 'out',
   server: {
-    url: 'https://relay-tan-three.vercel.app/dashboard',
+    url: 'https://relay-tan-three.vercel.app',
     cleartext: false
   }
 };
